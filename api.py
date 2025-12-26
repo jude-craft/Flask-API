87da4d9d9a2e4a56b639ea786cdd2a45fcf500c9
DevJude254 <derekjude254@gmail.com>
2025-12-26 21:15:00 +0300

    modified api.py

--- api.py
+++ api.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User(name =  {self.name}, email = {self.email})>'
+    
+    
+@app.route('/home')
 def home():
-    return '<h1>Welcome to the Home Page</h1>'
+    return 'Welcome to the Home Page'
 
 if __name__ == '__main__':
     app.run(debug=True)
